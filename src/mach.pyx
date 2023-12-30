fb8e15c4e274da65240746aea6dcb506a701e595
Christian Vogelgsang <chris@vogelgsang.org>
2023-12-30 16:43:29 +0100

    added Machine

--- src/mach.pyx
+++ src/mach.pyx
@@ -1,3 +1,18 @@
 include "pycpu.pyx"
 include "pymem.pyx"
 include "pytraps.pyx"
+
+cdef class Machine:
+  cdef readonly CPU cpu
+  cdef readonly Memory mem
+  cdef readonly Traps traps
+
+  def __cinit__(self, unsigned int cpu_type, unsigned ram_size_kb):
+    self.cpu = CPU(cpu_type)
+    self.mem = Memory(ram_size_kb)
+    self.traps = Traps()
+
+  def cleanup(self):
+    self.cpu.cleanup()
+    self.mem.cleanup()
+    self.traps.cleanup()
